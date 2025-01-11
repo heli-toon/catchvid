e871f41df0673e87d9075e5d973d7b22016775df
Salay Abdul Muhaimin Kanton <abdulkanton2005@gmail.com>
2025-01-11 20:51:06 +0000

    Refactor platform-specific output path handling and clean up unused imports

--- main/views.py
+++ main/views.py
@@ -5,7 +5,7 @@ from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 import os
 import platform
-from jnius import autoclass
+# from jnius import autoclass
 
 def on_progress(stream, bytes_remaining):
     total_size = stream.filesize
@@ -30,24 +30,23 @@ def home(request):
             stream = video.streams.get_lowest_resolution()
             stream.download()
 
+        
+            if platform.system() == 'Windows':
+                output_path = os.path.join(os.path.expanduser('~'), 'Downloads')
+            elif platform.system() == 'Linux':
+                output_path = os.path.join(os.path.expanduser('~'), 'Downloads')
+            elif platform.system() == 'Darwin':
+                output_path = os.path.join(os.path.expanduser('~'), 'Downloads')
+            # elif platform.system() == 'Android':
+            #     Environment = autoclass('android.os.Environment')
+            #     output_path = os.path.join(Environment.getExternalStorageDirectory().getPath(), 'Download')
+
+            else:
+                raise Exception('Unsupported operating system')
+
+            video = YouTube(link, on_progress_callback=on_progress)
+            video.download(output_path=output_path)
             return render(request, 'index.html')
-        
-        if platform.system() == 'Windows':
-            output_path = os.path.join(os.path.expanduser('~'), 'Downloads')
-        elif platform.system() == 'Linux':
-            output_path = os.path.join(os.path.expanduser('~'), 'Downloads')
-        elif platform.system() == 'Darwin':
-            output_path = os.path.join(os.path.expanduser('~'), 'Downloads')
-        elif platform.system() == 'Android':
-            Environment = autoclass('android.os.Environment')
-            output_path = os.path.join(Environment.getExternalStorageDirectory().getPath(), 'Download')
-
-        else:
-            raise Exception('Unsupported operating system')
-
-        video = YouTube(link, on_progress_callback=on_progress)
-        video.download(output_path=output_path)
-        
     except RegexMatchError:
         error_message = "Sorry, we couldn't find a YouTube video that matches your link. Please try again."
         return render(request, 'index.html', {'error_message': error_message})
